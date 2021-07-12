26ca58308b2a2d7831fa0ab1894288a9a40ee9f1
terry.tu <tuminfei1981@gmail.com>
2021-07-12 13:13:50 +0800

    add lockPointRate

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ https://faucet.matic.network
 
 BitflixNFT contract: 0x3C4Ed7d7CF1c405e19E2F7b0050549Ad7022E18D
 
-ERC20 token contract: 0x2d7882beDcbfDDce29Ba99965dd3cdF7fcB10A1e
+ERC20 token contract: 0x19d130656dE1c3f29B3F476D65fBE562a06fFcbf
 
 Marketplace contract: 0x0deF3509015E744Ba1F01582A3bc35981197968a
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,11 +4,13 @@
 const Marketplace = artifacts.require('core/Marketplace');
 const OtcMarketplace = artifacts.require('core/OtcMarketplace');
 const BitflixNFT = artifacts.require('ERC721/BitflixNFT');
+const USDT = artifacts.require('ERC20/USDT');
 
 function deployContracts(deployer) {
   deployer.deploy(Marketplace);
   deployer.deploy(OtcMarketplace);
   deployer.deploy(BitflixNFT);
+  deployer.deploy(USDT);
 }
 
 module.exports = deployContracts;
